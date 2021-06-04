24ee977910979c2b057b1346b91b3a344d5b6550
Anderson Marques <anderson_fisico@yahoo.com.br>
2021-06-04 18:46:05 -0300

    building pages

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,1 +1,14 @@
-import React from 'react'
+import './header.css'
+import imgNature from './imgNature.jpg'
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <div className="headerTitleSm">React & Node</div>
+        <div className="headerTitleLg">Blog</div>
+      </div>
+      <img className="headerImg" src={imgNature} alt="image" />
+    </div>
+  );
+}
